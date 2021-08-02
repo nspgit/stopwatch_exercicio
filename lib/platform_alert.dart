b031ceae1f02ef95b6d676ef56cb2b4602d77697
FlutLab.io <>
2021-08-02 16:26:53 +0000

    Initial commit

---
+++ lib/platform_alert.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class PlatformAlert {
+  final String title;
+  final String message;
+
+  const PlatformAlert({this.title, this.message})
+      // ignore: unnecessary_null_comparison
+      : assert(title != null),
+        assert(message != null);
+  void show(BuildContext context) {
+    final platform = Theme.of(context).platform;
+
+    if (platform == TargetPlatform.iOS) {
+      _buildCupertinoAlert(context);
+    } else {
+      _buildMaterialAlert(context);
+    }
+  }
+
+  void _buildMaterialAlert(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(title: Text(title), content: Text(message), actions: [
+            TextButton(child: Text('Close'), onPressed: () => Navigator.of(context).pop())
+          ]);
+        });
+  }
+
+  void _buildCupertinoAlert(BuildContext context) {
+    showCupertinoDialog(
+        context: context,
+        builder: (context) {
+          return CupertinoAlertDialog(title: Text(title), content: Text(message), actions: [
+            CupertinoButton(child: Text('Close'), onPressed: () => Navigator.of(context).pop())
+          ]);
+        });
+  }
+}
